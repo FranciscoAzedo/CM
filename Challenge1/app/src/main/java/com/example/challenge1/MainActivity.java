5eb69b98e8342e39f0409bfe9b5c3c11d9eaa482
Francisco Azedo <franciscoazedo97@gmail.com>
2020-10-07 23:35:21 +0100

    Labels with animal information

--- Challenge1/app/src/main/java/com/example/challenge1/MainActivity.java
+++ Challenge1/app/src/main/java/com/example/challenge1/MainActivity.java
@@ -7,11 +7,32 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private ImageView imageView;
     private Spinner spinner;
+    private TextView ownerName;
+    private TextView animalName;
+    private TextView animalAge;
+
+    private ArrayList<Animal> animals = new ArrayList<Animal>(){
+        {
+            add(new Animal("cat", "Nelsu", "Tobias", 4));
+            add(new Animal("dog", "Manel", "Sissi", 3));
+            add(new Animal("duck", "Jeremias", "Alfredo", 2));
+            add(new Animal("elephant", "Lipito", "Trombas", 14));
+            add(new Animal("fish", "Cidito", "Fixe", 10));
+            add(new Animal("giraffe", "Celito", "Cumprido", 8));
+            add(new Animal("horse", "Litito", "Tarolo", 9));
+            add(new Animal("lion", "Acacio", "Jubinha", 24));
+            add(new Animal("rabbit", "Armenioc", "Saltitao", 5));
+            add(new Animal("tiger", "Junioc", "Dentes", 7));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +40,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
+        ownerName = findViewById(R.id.ownerName);
+        animalName = findViewById(R.id.animalName);
+        animalAge = findViewById(R.id.animalAge);
+
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        imageView.setImageResource(getResourceByName(spinner.getSelectedItem().toString(), "drawable"));
+        Animal currentAnimal = getAnimalByType(spinner.getSelectedItem().toString());
+
+        imageView.setImageResource(
+                Utils.getResourceByName(getResources(),
+                        currentAnimal.getType(),
+                        "drawable",
+                        getPackageName())
+        );
+        ownerName.setText(currentAnimal.getOwner());
+        animalName.setText(currentAnimal.getName());
+        animalAge.setText(String.valueOf(currentAnimal.getAge()));
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) { }
 
-    private int getResourceByName(String resourceName, String resourceType) {
-        return getResources().getIdentifier(resourceName.toLowerCase(), resourceType, getPackageName());
+    private Animal getAnimalByType(String type){
+        for (Animal animal : animals) {
+            if (animal.getType().equalsIgnoreCase(type))
+                return animal;
+        }
+        return null;
+    }
+
+    public void editInfo(View view) {
+
     }
 }
