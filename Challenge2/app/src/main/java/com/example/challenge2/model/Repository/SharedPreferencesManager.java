87b2d44f47a73734ddd744845f160fadc62b39e9
gclarobatista <34254160+gclarobatista@users.noreply.github.com>
2020-11-10 15:25:53 +0000

    SharedPreferencesManager implemented

--- Challenge2/app/src/main/java/com/example/challenge2/model/Repository/SharedPreferencesManager.java
+++ Challenge2/app/src/main/java/com/example/challenge2/model/Repository/SharedPreferencesManager.java
@@ -1,4 +1,22 @@
 package com.example.challenge2.model.Repository;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.challenge2.R;
+
 public class SharedPreferencesManager {
+
+    public static String getSharedPreference(Context context, final String key, final String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name),
+                Context.MODE_PRIVATE);
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public static void saveSharedPreference(Context context, String key, String value) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(context.getString(R.string.app_name),
+                Context.MODE_PRIVATE).edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
 }
